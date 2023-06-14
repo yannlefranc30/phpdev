18fbde1e187f7c56d46cbc49179e4259bdc04ff7
yann <yannlefranc330@gmail.com>
2023-06-14 08:52:34 +0200

    phpdev

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+class greeting {
+    public static function  dev() {
+        echo "devloppeur";
+    }
+}
+// Call static method
+greeting::dev();
+?>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
